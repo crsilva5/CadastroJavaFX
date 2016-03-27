cd0e82d9dd24e4cd81ae669440a35a55dfd379d8
cleiton <cl3iton357@gmailcom>
2016-03-27 20:13:21 -0300

    Usar o DateUtil

--- src/ch/makery/address/view/PersonOverviewController.java
+++ src/ch/makery/address/view/PersonOverviewController.java
@@ -6,6 +6,7 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import ch.makery.address.MainApp;
 import ch.makery.address.model.Person;
+import ch.makery.address.util.DateUtil;
 
 public class PersonOverviewController {
     @FXML
@@ -43,25 +44,7 @@ public class PersonOverviewController {
      * @param person a pessoa ou null
      */
     private void showPersonDetails(Person person) {
-        if (person != null) {
-            // Preenche as labels com informações do objeto person.
-            firstNameLabel.setText(person.getFirstName());
-            lastNameLabel.setText(person.getLastName());
-            streetLabel.setText(person.getStreet());
-            postalCodeLabel.setText(Integer.toString(person.getPostalCode()));
-            cityLabel.setText(person.getCity());
-
-            // TODO: Nós precisamos de uma maneira de converter o aniversário em um String! 
-            // birthdayLabel.setText(...);
-        } else {
-            // Person é null, remove todo o texto.
-            firstNameLabel.setText("");
-            lastNameLabel.setText("");
-            streetLabel.setText("");
-            postalCodeLabel.setText("");
-            cityLabel.setText("");
-            birthdayLabel.setText("");
-        }
+    	birthdayLabel.setText(DateUtil.format(person.getBirthday()));
     }
     
     public PersonOverviewController() {
